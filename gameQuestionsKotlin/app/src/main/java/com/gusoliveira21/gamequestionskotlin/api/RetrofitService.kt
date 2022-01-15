284a9b1cfe375c29fce74c4f7649b6a25180de98
gusoliveira21 <42920754+gusoliveira21@users.noreply.github.com>
2022-01-15 07:28:28 -0300

    adiciona teste de conexão

--- gameQuestionsKotlin/app/src/main/java/com/gusoliveira21/gamequestionskotlin/api/RetrofitService.kt
+++ gameQuestionsKotlin/app/src/main/java/com/gusoliveira21/gamequestionskotlin/api/RetrofitService.kt
@@ -6,10 +6,6 @@ import retrofit2.http.GET
 
 interface RetrofitService {
 
-    //https://raw.githubusercontent.com/gusoliveira21/Game-Quiz-Kotlin/main/lista-perguntas.txt
-    //http://webservice.eu5.org/quiz_kotlin/quiz_kotlin_questions.html
-
-
     @GET("/gusoliveira21/Game-Quiz-Kotlin/main/lista-perguntas")
     fun list():Call<MutableList<QuestionsKotlin>>
 
--- gameQuestionsKotlin/app/src/main/java/com/gusoliveira21/gamequestionskotlin/view/HomeFragment.kt
+++ gameQuestionsKotlin/app/src/main/java/com/gusoliveira21/gamequestionskotlin/view/HomeFragment.kt
@@ -1,12 +1,19 @@
 package com.gusoliveira21.gamequestionskotlin.view
 
+import android.content.Context
+import android.graphics.Color
+import android.net.ConnectivityManager
 import android.os.Bundle
 import android.view.*
+import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
+import com.google.android.material.snackbar.Snackbar
 import com.gusoliveira21.gamequestionskotlin.R
 import com.gusoliveira21.gamequestionskotlin.databinding.FragmentHomeBinding
+
 
 class HomeFragment : Fragment() {
     private val binding by lazy { FragmentHomeBinding.inflate(LayoutInflater.from(context)) }
@@ -31,14 +38,49 @@ class HomeFragment : Fragment() {
                 super.onOptionsItemSelected(item)
     }
 
-    private fun listeners() {
-        binding.btPlay.setOnClickListener {
-            it.findNavController().navigate(
-                HomeFragmentDirections.actionHomeFragmentToGameFragment()
-            )
+    fun isOnline(context: Context): Boolean {
+        val connectivity_manager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val active_network = connectivity_manager.activeNetworkInfo
+        if (active_network != null) {
+            return true// connected to the internet
+            if (active_network.type == ConnectivityManager.TYPE_WIFI) {
+                return true// connected to wifi
+            } else if (active_network.type == ConnectivityManager.TYPE_MOBILE) {
+                return true// connected to mobile data
+            }
+        } else {
+            return false// not connected to the internet
         }
     }
 
+
+     fun onSNACK(view: View){
+         //Snackbar(view)
+         val snackbar = Snackbar.make(view, "Sem conexão com a internet!",
+             Snackbar.LENGTH_LONG).setAction("Action", null)
+         snackbar.setActionTextColor(Color.YELLOW)
+         val snackbarView = snackbar.view
+         snackbarView.setBackgroundColor(Color.BLACK)
+         val textView =
+             snackbarView.findViewById(com.google.android.material.R.id.snackbar_text) as TextView
+         textView.setTextColor(Color.YELLOW)
+         textView.textSize = 28f
+         snackbar.show()
+     }
+
+    private fun listeners() {
+        binding.btPlay.setOnClickListener {
+            if (isOnline(requireContext())) {
+                it.findNavController().navigate(
+                    HomeFragmentDirections.actionHomeFragmentToGameFragment()
+                )
+            } else
+                onSNACK(it)
+                //Toast.makeText(requireContext(), "Sem conexão", Toast.LENGTH_LONG).show()
+        }
     }
+
+}
 
 
