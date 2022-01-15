5fbe5f1ea1051b4cadb7fb1a3d59b0978236455c
gusoliveira21 <42920754+gusoliveira21@users.noreply.github.com>
2022-01-15 05:38:41 -0300

    atualiza

--- gameQuestionsKotlin/app/src/main/java/com/gusoliveira21/gamequestionskotlin/api/RetrofitInicializer.kt
+++ gameQuestionsKotlin/app/src/main/java/com/gusoliveira21/gamequestionskotlin/api/RetrofitInicializer.kt
@@ -6,6 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 class RetrofitInicializer {
     companion object{
         const val BASE_URL = "https://raw.githubusercontent.com/"
+        //const val BASE_URL = "http://webservice.eu5.org"
     }
     private fun questionProvider():Retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
